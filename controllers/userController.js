1c13dde7e754fd0166c69c53a270dff08f4c2f83
goncalhurtado <goncalhurtado@gmail.com>
2024-01-27 12:53:59 -0300

    added basic config

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+
+
+
+
+const getAllUsers = (req, res) => {
+    res.send({
+    "mensaje": "todos los usuiarios fueron encontrados"
+    })
+}
+
+module.exports = {
+    getAllUsers
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const router = require('./routes');
+// const dbConnection = require('./database/db');
+// const cloudinary = require('cloudinary').v2;
+// const passport = require('passport');
+// const jwtStrategy = require('./passport/jwt');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+//passport
+// passport.use("jwt", jwtStrategy);
+
+// cloudinary config
+
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+
+
+// configuracion rutas
+app.use(process.env.API, router)
+
+// puerto
+const port = process.env.PORT;
+
+// dbConnection();
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
