eac4801ce153ecb6a9b481613c3ae8fb99e3d000
goncalhurtado <goncalhurtado@gmail.com>
2024-03-15 12:42:29 -0300

    add conversation routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,23 @@ const {
   getConversation,
   newConversation,
 } = require("../controllers/conversationController");
+const {
+  postMessage,
+  getMessages,
+} = require("../controllers/messageController");
 
-router.get("/users", getAllUsers);
+//users
+
 router.post("/user", registerUser);
+router.get("/users", getAllUsers);
 
 // conversation
 
-router.get("/conversations/:userId", getConversation);
 router.post("/conversation", newConversation);
+router.get("/conversations/:userId", getConversation);
+
+// messages
+router.post("/message", postMessage);
+router.get("/messages/:conversationId", getMessages);
 
 module.exports = router;
